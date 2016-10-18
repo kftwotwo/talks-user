1cfaefa9758d167a442616bfa62ec1959d15c57b
Kevin Finley and Luke Philips <kevin@>
2016-10-18 13:34:10 -0700

    add goodbye button

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Hello how are you?</li>");
     $("ul#computer").prepend("<li>I'm doing swell.");
   });
+  $("#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye my only friend!</li>");
+    $("ul#computer").prepend("<li>I never liked you anyway.</li>");
+  });
 });
