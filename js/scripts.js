478b31dca6a260704ae23f1d3a1df5b1e889c718
Kevin Finley and Luke Philips <kevin@>
2016-10-18 13:31:03 -0700

    add hello button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-
+  $("#hello").click(function() {
+    $("ul#user").prepend("<li>Hello how are you?</li>");
+    $("ul#computer").prepend("<li>I'm doing swell.");
+  });
 });
